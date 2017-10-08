23b55d7ec64ea7358910db3bac9ae03c37ca77f1
joyvalencia <32458789+joyvalencia@users.noreply.github.com>
2017-10-08 19:14:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     $suma=$var1+$var2; // Ali
     $resta=$var1-$var2; // Joy
     $multiplicacion=$var1*$var2; // Cristina
-    $division; 
+    $division=$var1/$var2; //conflicto
 
     echo "resultado sumar: ".$suma ; 
     echo "<br>"; 
