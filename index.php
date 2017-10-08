243acc606d07ee549d6f413b96bedc4686b4cb4f
Ali <Ali@AliEM>
2017-10-08 18:01:25 +0200

    C inicial

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+    $var1=5; 
+    $var2=4; 
+
+    $suma=$var1+$var2; 
+    $resta=$var1-$var2; 
+    $multiplicacion=$var1*$var2; 
+    $division=$var1/$var2; 
+
+    echo "resultado sumar: ".$suma ; 
+    echo "<br>"; 
+    echo "resultado resta: ".$resta; 
+    echo "<br>"; 
+    echo "resultado multiplicacion: ".$multiplicacion; 
+    echo "<br>"; 
+    echo "resultado division: ".$division; 
+
+?>
